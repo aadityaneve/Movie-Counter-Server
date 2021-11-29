afc9649b5742a16ad6eb52f98fe92b3202b598b0
Aditya Neve <aditya.neve@gmail.com>
2021-11-29 22:33:58 +0530

    server will send visitor count in response

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/counter", async (req, res) => {
         const counter = await VisitorCounter.findById(
             "61a4bf9f17e63e3fe8c33d8c"
         );
-        return res.status(201).send(counter);
+        return res.status(201).send(counter.visitor_count);
     } catch (e) {
         return res.status(500).send({ message: e.message });
     }
@@ -40,7 +40,7 @@ app.patch("/counter", async (req, res) => {
             { $inc: { visitor_count: 1 } },
             { new: true }
         );
-        return res.status(201).send(counter);
+        return res.status(201).send(counter.visitor_count);
     } catch (e) {
         return res.status(500).send({ message: e.message });
     }
